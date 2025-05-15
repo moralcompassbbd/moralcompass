1c20d13576654bd0ee810313f0cbb11a23ad40f1
Michael Chinyama <michael.chinyama@bbd.co.za>
2025-05-15 16:03:13 +0200

    updated to match master

--- client/src/homepage.ts
+++ client/src/homepage.ts
@@ -24,22 +24,40 @@ async function addManagerCards(){
             img.src = "/static/manager.png";
             img.ariaHidden = "true";
     
-            const h2 = document.createElement('h2');
-            h2.textContent = "⚙️ Question Manager";
-    
-            const p = document.createElement('p');
-            p.textContent = "Manage quiz questions"; 
+            const h2_questions = document.createElement('h2');
+            h2_questions.textContent = "⚙️ Question Manager";
+
+            const p_questions = document.createElement('p');
+            p_questions.textContent = "Manage quiz questions"; 
             
-            const homepageCardItem = document.createElement('li');
-            homepageCardItem.id = "card-manager"
-            homepageCardItem.className = "bg-pastel-purple";
-            homepageCardItem.onclick = () => SPA.navigatePage('manager');
+            const questionCardItem = document.createElement('li');
+            questionCardItem.id = "card-manager"
+            questionCardItem.className = "bg-pastel-purple";
+            questionCardItem.onclick = () => SPA.navigatePage('manager');
             
-            homepageCardItem.appendChild(img);
-            homepageCardItem.appendChild(h2);
-            homepageCardItem.appendChild(p);
-    
-            homepageCardList.appendChild(homepageCardItem);
+            questionCardItem.appendChild(img);
+            questionCardItem.appendChild(h2_questions);
+            questionCardItem.appendChild(p_questions);
+
+            //
+
+            const h2_users = document.createElement('h2');
+            h2_users.textContent = "🙋 Users";
+            
+            const p_users = document.createElement('p');
+            p_users.textContent = "Manage users"; 
+
+            const userCardItem = document.createElement('li');
+            userCardItem.id = "card-users"
+            userCardItem.className = "bg-pastel-purple";
+            userCardItem.onclick = () => SPA.navigatePage('user_tables');
+
+            userCardItem.appendChild(img);
+            userCardItem.appendChild(h2_users);
+            userCardItem.appendChild(p_users);
+            
+            homepageCardList.appendChild(questionCardItem);
+            homepageCardList.appendChild(userCardItem);
         } else{
             // don't even bother to render the manager button
         }
